cb0a79d3641fe44afe2ab789cab4d05e4e58d4d0
Hardy Jones <hardy@noredink.com>
2017-05-26 09:23:50 -0600

    Step 1: Create a working web server

--- src/Blog.purs
+++ src/Blog.purs
@@ -1,8 +1,19 @@
 module Blog where
 
+import Control.IxMonad ((:*>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
 import Data.Unit (Unit)
+import Hyper.Node.Server (defaultOptionsWithLogging, runServer)
+import Hyper.Response (closeHeaders, respond, writeStatus)
+import Hyper.Status (statusOK)
+import Node.HTTP (HTTP)
 
-main :: forall e. Eff ( console :: CONSOLE | e ) Unit
-main = log "Hello LambdaConf!"
+main :: forall e. Eff (console :: CONSOLE, http :: HTTP | e) Unit
+main =
+  runServer defaultOptionsWithLogging {} blog
+  where
+  blog =
+    writeStatus statusOK
+      :*> closeHeaders
+      :*> respond "Hello, LambdaConf!"
